6b9a1188cfffe38f2799e8da3b270e5fba2b3f08
John Doe RO <JohnDoe@Doe.ro>
2018-03-07 11:38:26 +0100

    Creation classe Game

--- app/src/main/java/android/cambiok/ballsgame/Game.java
+++ app/src/main/java/android/cambiok/ballsgame/Game.java
@@ -4,5 +4,18 @@ package android.cambiok.ballsgame;
  * Created by bat on 07/03/2018.
  */
 
+/*
+Variables à faire :
+    - Joueur 1 & Joueur 2
+    - Tableau
+    -
+
+Methodes à faire :
+    - GenerationAleatoireTableau
+    - TrouverVoisin
+    - ResetGame
+*/
+
 public class Game {
+
 }
