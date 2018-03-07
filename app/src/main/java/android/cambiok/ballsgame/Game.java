66f4ccee0608d8e7622875f10dc3a5e1de675b5e
John Doe RO <JohnDoe@Doe.ro>
2018-03-07 12:28:44 +0100

    --

--- app/src/main/java/android/cambiok/ballsgame/Game.java
+++ app/src/main/java/android/cambiok/ballsgame/Game.java
@@ -1,5 +1,7 @@
 package android.cambiok.ballsgame;
 
+import java.util.Random;
+
 /**
  * Created by bat on 07/03/2018.
  */
@@ -26,8 +28,29 @@ public class Game {
 
     }
 
+    // void GenerationAleatoireTableau(int difficulte) {
+    // Tab = -1 -> case vide
+    // Tab = [0; difficulte] -> case pleine avec couleur [0; difficulte]
     void GenerationAleatoireTableau(int difficulte) {
         // ..
+
+        int x, y;
+
+
+        // https://www.compilejava.net/
+        //
+        int randomNumber = r.ints(1, 0, 11).findFirst().getAsInt();
+
+
+        for (i = 0; i < ..; i++) {
+            for (y = 0; y < ..; y++){
+                int randomNumber = r.ints(1, difficulte, 11).findFirst().getAsInt();
+                Tableau[x][y] = randomNumber;
+            }
+        }
+
+
+
     }
 
     int [] [] TrouverVoisin() {
@@ -38,6 +61,16 @@ public class Game {
 
     int [][] getTableau()  {
         return Tableau;
+    }
+
+    void initTableau()  {
+        int x, y;
+
+        for (i = 0; i < ..; i++) {
+            for (y = 0; y < ..; y++){
+                Tableau[x][y] = -1;
+            }
+        }
     }
 
 }
