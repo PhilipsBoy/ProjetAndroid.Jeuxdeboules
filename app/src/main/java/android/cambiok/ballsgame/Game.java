f38e27d829fcd127fda607661882d8f6b0fb5928
John Doe RO <JohnDoe@Doe.ro>
2018-04-06 15:50:00 +0200

    Modif classes

--- app/src/main/java/android/cambiok/ballsgame/Game.java
+++ app/src/main/java/android/cambiok/ballsgame/Game.java
@@ -210,7 +210,7 @@ public class Game {
                 if (GetCaseColor(x, y) == -1) {
                     // On decale tous vers le bas
 
-                    for (x2 = x; x2 >= 0; x2--) {
+                    for (x2 = x; x2 + 1 < getTailleX(); x2++) {
 
                         // On recupère la couleur de la case de dessus
                         if ((x2 + 1) < getTailleX()) {
