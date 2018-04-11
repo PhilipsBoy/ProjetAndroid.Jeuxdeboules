80650d9ac1b6e2d43e6c08f19419ea4613bb1dd8
John Doe RO <JohnDoe@Doe.ro>
2018-04-11 20:58:16 +0200

    Modif classes

--- app/src/main/java/android/cambiok/ballsgame/Joueur.java
+++ app/src/main/java/android/cambiok/ballsgame/Joueur.java
@@ -4,17 +4,25 @@ package android.cambiok.ballsgame;
  * Created by bat on 07/03/2018.
  */
 
+// Classe joueur
+// Permet de décrire un joueur
+// Name : pseudo du joueur.
+// Score : score du joueur.
+// ScoreLevel : score du joueur en entrée de lvl.
 public class Joueur {
+    // Variables
     int score;
     int scoreLevel;
     String Name;
 
-    public Joueur(String name) { // Constructeur
+    // Constructeur
+    public Joueur(String name) {
         setScore(0);
         setScoreLevel(0);
         setName(name);
     }
 
+    // get / set
     int getScore() {return score;}
     void setScore(int scoreJoueur) {score = scoreJoueur;}
 
